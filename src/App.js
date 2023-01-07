21e68957cdc7705704ccfa2b00392d8c21e21c9e
RafaelHDSV <rafaebolt@gmail.com>
2023-01-07 15:40:33 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className='title'>Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder='Digite seu CEP...' />
+
+        <button className='btn_search'>Procurar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
